5c3876fae76e53de59ba4301fc132708bd27701d
cauahu <andahuzhuang@163.com>
2016-04-19 21:05:45 +0800

    add allkindofsort

--- allkindofsort/main.cpp
+++ allkindofsort/main.cpp
@@ -3,6 +3,59 @@
 
 using namespace std;
 
+
+void shellsort(int *a, int n)
+{
+	int d=n/2;
+	while(d > 0)
+	{
+		for(int i=d;i<n;i++)
+		{
+			if(a[i-d] > a[i])
+				swap(a[i-d], a[i]);
+		}
+		d /= 2;
+	}
+
+}
+
+void quiksort(int *a, int b, int e)
+{
+	int i = b , j = e, temp ;
+	if(i<j)
+	{
+		while(i<j)
+		{
+			temp = a[i];
+			while(i<j&& temp < a[j])
+				j--;
+			if(i<j)
+				a[i++] = a[j];
+			while(i<j&& temp > a[i])
+				i++;
+			if(i<j)
+				a[j--] = a[i];
+		}
+		a[i] = temp;
+		quiksort(a,b,i-1);
+		quiksort(a,i+1,e);
+	}
+}
+
+
+void bubblingsort(int *a, int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=n-1;j>i;j--)
+		{
+			if(a[j] < a[j-1])
+				swap(a[j], a[j-1]);
+		}
+	}
+}
+
+
 void selectsort(int *a, int n)
 {
     /*
@@ -46,7 +99,7 @@ void radixsort(int *a, int n)
     /*
 
     */
-    vector<queue<int>> RAD(10);
+    vector<queue<int> > RAD(10);
     bool flag = false;
     int carry = 10;
     int r = 2;
@@ -77,8 +130,12 @@ int main()
     //int a[] = {6,2,7,4,1,8,3,5};
     //selectsort(a,8);
     int a[] = {73, 22, 93, 43, 55, 14, 28, 65};
-    radixsort(a,8);
+    //radixsort(a,8);
+	//bubblingsort(a,8);
+	//shellsort(a,8);
+	quiksort(a,0,7);
     for(int i=0;i<8;i++)
         cout << a[i] << " ";
+	cout << endl;
     return 0;
 }
